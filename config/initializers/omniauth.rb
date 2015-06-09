ed00ab45fa4da2b6fb67903b816fef8eaa6cc3c4
Amy Bertken <amy.bertken@gmail.com>
2015-06-09 14:56:41 -0600

    remove dotenv reference in omniauth initializers

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,3 @@
-require 'dotenv'
-Dotenv.load
-
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :instagram, ENV['INSTAGRAM_ID'], ENV['INSTAGRAM_SECRET'], provider_ignores_state: true
 end
